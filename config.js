383f4d3334cd324c67e4b4efef95a6aeba34099c
Dexter <63425800+DexterTheDev@users.noreply.github.com>
2022-04-10 17:32:06 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.port=8080
+exports.token= "OTEwMTU2ODA2OTg5NDE0NDUw.YZOvlA.q1SOM6X4dTqHiNbt5G6rUjJl2Ws"
+exports.domain= "http://localhost:8080"
+exports.clientSecret= "5F9RjMkZcgptpB9uCqvMxJPM_vq5buvM"
+exports.id="910156806989414450" 
+exports.owners= []
+exports.mongo_url= "mongodb+srv://Dexter:admin53645364@dsccord.niywd.mongodb.net/vapor?retryWrites=true&w=majority"
