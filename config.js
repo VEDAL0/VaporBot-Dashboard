a9d1d6c05a0f58e382d812e2b26023e897637ae2
Dexter <63425800+DexterTheDev@users.noreply.github.com>
2022-04-10 17:32:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.port=8080
-exports.token= "OTEwMTU2ODA2OTg5NDE0NDUw.YZOvlA.q1SOM6X4dTqHiNbt5G6rUjJl2Ws"
+exports.token= ""
 exports.domain= "http://localhost:8080"
-exports.clientSecret= "5F9RjMkZcgptpB9uCqvMxJPM_vq5buvM"
-exports.id="910156806989414450" 
+exports.clientSecret= ""
+exports.id="" 
 exports.owners= []
-exports.mongo_url= "mongodb+srv://Dexter:admin53645364@dsccord.niywd.mongodb.net/vapor?retryWrites=true&w=majority"
+exports.mongo_url= ""
